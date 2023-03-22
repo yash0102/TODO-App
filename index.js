b6b4009e4c6e6f619cf60dff7468ce3630ee499d
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-03-22 16:57:52 +0530

    Update index.js and make index.ejs for html structure

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// middleware
 
+// added a parser, it signifies the middleware
+app.use(express.urlencoded()); 
+
+// for getting static files
+app.use(express.static("assets"));
+
+app.use(expressLayout);
+
+// set template/view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+
+
+
+// Returning response from the server
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.render('index', {
+        title: 'TODOList'
+    });
 });
+
+app.get
 
 // listening request from express server
 app.listen(port, (err)=>{
