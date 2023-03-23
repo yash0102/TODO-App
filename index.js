a072bf95dba7a07d8139800f9e256cd01904aaa7
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-03-24 02:32:22 +0530

    added routes and controller

--- index.js
+++ index.js
@@ -2,31 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+
+
 // middleware
 
 // added a parser, it signifies the middleware
 app.use(express.urlencoded()); 
 
-// for getting static files
-app.use(express.static("assets"));
-
-app.use(expressLayout);
-
-// set template/view engine
-app.set("view engine", "ejs");
-app.set("views", "./views");
 
 
 
+app.use('/', require('./routes'));// any request comes,
 
-// Returning response from the server
-app.get('/',(req,res)=>{
-    res.render('index', {
-        title: 'TODOList'
-    });
-});
 
-app.get
 
 // listening request from express server
 app.listen(port, (err)=>{
